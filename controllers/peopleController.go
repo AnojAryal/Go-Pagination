803940d9872e5888522ada6ebe4a167568c87f8a
AnojAryal <anoj1810@gmail.com>
2024-06-30 12:34:29 +0545

    adding previous, next and current page

--- controllers/peopleController.go
+++ controllers/peopleController.go
@@ -1,20 +1,64 @@
 package controllers
 
 import (
+	"math"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/robbyklein/pages/initializers"
 	"github.com/robbyklein/pages/models"
 )
 
+type PaginationData struct {
+	NextPage     int
+	PreviousPage int
+	CurrentPage  int
+	TotalPages   int
+}
+
 func PeopleIndexGET(c *gin.Context) {
-	// Get the people
+	// Get page number
+	perPage := 10
+	page := 1
+	pageStr := c.Param("page")
+
+	if pageStr != "" {
+		page, _ = strconv.Atoi(pageStr)
+	}
+
+	if page < 1 {
+		page = 1
+	}
+
+	// Calculate total pages
+	var totalRows int64
+	result := initializers.DB.Model(&models.Person{}).Count(&totalRows)
+	if result.Error != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Failed to count people"})
+		return
+	}
+	totalPages := int(math.Ceil(float64(totalRows) / float64(perPage)))
+
+	// Calculate offset
+	offset := (page - 1) * perPage
+
+	// Get people for the current page
 	var people []models.Person
-	initializers.DB.Find(&people)
+	result = initializers.DB.Limit(perPage).Offset(offset).Find(&people)
+	if result.Error != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch people"})
+		return
+	}
 
 	// Render the page
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
 		"people": people,
+		"pagination": PaginationData{
+			NextPage:     page + 1,
+			PreviousPage: page - 1,
+			CurrentPage:  page,
+			TotalPages:   totalPages,
+		},
 	})
 }
